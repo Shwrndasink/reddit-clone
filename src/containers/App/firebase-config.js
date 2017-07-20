a0304bbd2536751442115d7c6dbb3056a566fa53
Marc White <mwhite@overstock.com>
2017-07-19 18:01:00 -0600

    got firebase database to console.log

---
+++ src/containers/App/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCkwT37qXfMQR9pb5mZpFdIshluBfUG6o0",
+    authDomain: "reddit-clone-9dd7e.firebaseapp.com",
+    databaseURL: "https://reddit-clone-9dd7e.firebaseio.com",
+    projectId: "reddit-clone-9dd7e",
+    storageBucket: "reddit-clone-9dd7e.appspot.com",
+    messagingSenderId: "95551807857"
+};
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,8 +1,34 @@
 // src/containers/App/index.js
 
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
+import config from './firebase-config';
 
 class App extends Component {
+  constructor(){
+    super();
+    firebase.initializeApp(config);
+  }
+
+  state = {
+    posts: [],
+    loading: false
+  };
+
+  componentWillMount() {
+    let postsRef = firebase.database().ref('posts');
+    let _this = this;
+
+    postsRef.on('value', function(snapshot){
+      console.log(snapshot.val());
+
+      _this.setState({
+        posts: snapshot.val(),
+        loading: false
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
